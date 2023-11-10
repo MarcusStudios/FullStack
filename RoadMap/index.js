bb7a13540453e8855a3524add66306579c746a63
marcus665 <125776824+marcus665@users.noreply.github.com>
2023-11-10 13:59:10 -0300

    alteraçao index.js road map 10/11 13:58

---
+++ RoadMap/index.js
@@ -0,0 +1,9 @@
+// Defina os números que você deseja somar
+var numero1 = 5;
+var numero2 = 10;
+
+// Realize a soma usando o operador de adição
+var resultado = numero1 + numero2;
+
+// Exiba o resultado no console
+console.log("A soma é: " + resultado);
